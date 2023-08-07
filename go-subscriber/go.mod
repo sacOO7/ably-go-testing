2e8fe9f7a4ae5246db4995d69449d4fc0e4dd262
sacOO7 <sachinshinde7676@gmail.com>
2023-08-07 23:40:34 +0530

    Created test project for go subscriber

---
+++ go-subscriber/go.mod
@@ -0,0 +1,12 @@
+module gosubscriber
+
+go 1.19
+
+require github.com/ably/ably-go v1.2.13
+
+require (
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
---
+++ go-subscriber/main.go
@@ -0,0 +1,42 @@
+package gosubscriber
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/ably/ably-go/ably"
+)
+
+func main() {
+
+	var (
+		ABLY_KEY    = ""
+		channelName = "channel"
+	)
+
+	client, err := ably.NewRealtime(
+		ably.WithKey(ABLY_KEY),
+		ably.WithLogLevel(ably.LogVerbose),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	channel := client.Channels.Get(channelName)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	u, err := channel.SubscribeAll(ctx, func(m *ably.Message) {
+		log.Println("%v", m)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer u()
+
+	<-ctx.Done()
+}
